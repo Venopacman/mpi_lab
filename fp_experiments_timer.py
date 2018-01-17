2e2ec6b2d9877b8bb3871c57744edda3c9010468
Venopacman <pdsmirnov@yandex.ru>
2018-01-17 18:44:24 +0300

    wwrite all computations without averaging

--- fp_experiments_timer.py
+++ fp_experiments_timer.py
@@ -10,18 +10,16 @@ cpu_range = range(1, 50)
 
 
 def main():
-    fp_exp_results = pd.DataFrame(columns=['Algorithm', 'Cores', 'AVG_time'])
+    fp_exp_results = pd.DataFrame(columns=['Algorithm', 'Cores', 'time'])
     for exp, cpu in itertools.product(experiments_list, cpu_range):
-        avg_time = 0.0
-        for i in range(25):
+        for i in range(40):
             start = timer()
             msg = os.system(
-                "mpiexec -q -n {1} python3 /home/psmirnov/repos/mpi_lab/{0} 0.0 1.0 30000 None".format(exp, cpu))
+                "mpiexec -q -n {1} python3 /home/psmirnov/repos/mpi_lab/{0} 0.0 1.0 500000 None".format(exp, cpu))
             end = timer()
-            avg_time += end - start
-        avg_time /= 25
-        fp_exp_results = fp_exp_results.append(
-            {'Algorithm': exp, 'Cores': cpu, 'AVG_time': avg_time}, ignore_index=True)
+            fp_exp_results = fp_exp_results.append(
+                {'Algorithm': exp, 'Cores': cpu, 'time': timer() - start}, ignore_index=True)
+
     fp_exp_results.to_csv('fp_experiment_cluster.csv', index=False)
 
 
--- sp_experiments_timer.py
+++ sp_experiments_timer.py
@@ -7,26 +7,20 @@ import numpy as np
 experiments_list = ['gauss.py', 'rectangular.py',
                     'simpson.py', 'trapesoidal.py']
 
-cpu_range = range(20, 21)
-
-B_range = np.linspace(1.0, 10.0, 25)
+B_range = np.linspace(1.0, 10.0, 40)
 
 
 def main():
     fp_exp_results = pd.DataFrame(
         columns=['Algorithm', 'Cores', 'N(b)', 'Integration_step', 'AVG_time'])
-    cpu = 20
+    cpu = 8
     for exp, b in itertools.product(experiments_list, B_range):
-        avg_time = 0.0
-        for i in range(25):
+        for i in range(40):
             start = timer()
             msg = os.system(
-                "mpiexec -q -n {1} python3 /home/psmirnov/repos/mpi_lab/{0} 0.0 {2} 1 0.001".format(exp, cpu, b))
-            end = timer()
-            avg_time += end - start
-        avg_time /= 25
-        fp_exp_results = fp_exp_results.append(
-            {'Algorithm': exp, 'Cores': cpu, 'AVG_time': avg_time, 'N(b)': b, 'Integration_step': 0.001}, ignore_index=True)
+                "mpiexec -q -n {1} python3 /home/psmirnov/repos/mpi_lab/{0} 0.0 {2} 1 0.0005 ".format(exp, cpu, b))
+            fp_exp_results = fp_exp_results.append(
+                {'Algorithm': exp, 'Cores': cpu, 'time': timer() - start, 'N(b)': b, 'Integration_step': 0.001}, ignore_index=True)
     fp_exp_results.to_csv('sp_experiment_cluster.csv', index=False)
 
 
